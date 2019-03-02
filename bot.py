d0e63a86bba4b807913b1ea5dd8c75f2b56d2c04
Mattias Nystrom <35585644+mat7ias@users.noreply.github.com>
2019-03-02 16:23:33 +1100

    fixes and change prefix to !

--- bot.py
+++ bot.py
@@ -3,105 +3,113 @@ import yaml
 import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
+from aiohttp import AsyncResolver, ClientSession, TCPConnector
 import asyncio
 import time
 
+#open config file
 config = None
-if os.path.isfile("config.yaml"):
-    with open("config.yaml") as config_file:
+if os.path.isfile("bot/Draidenbot/config.yaml"):
+    with open("bot/Draidenbot/config.yaml") as config_file:
         config = yaml.load(config_file)
 else:
     exit("No configuration file 'config.yaml' found")
     sys.exit()
 
-
-Client = discord.Client()
+#start bot    
 client = commands.Bot(command_prefix = "!")
-
+client.http_session = ClientSession(connector=TCPConnector(resolver=AsyncResolver()))
 @client.event
 async def on_ready():
     print("Bot is online and connected to Discord")
 
+#commands    
 @client.event
 async def on_message(message):
   userID = message.author.id
   if message.content.upper().startswith('!PING'):
     await client.send_message(message.channel, "<@%s> Pong!" % (userID))
 
-  if message.content.lower().startswith('/commands'):
+  if message.content.lower().startswith('!commands'):
     msg = config['commands']
     await client.send_message(message.channel, msg)
 
-  if message.content.lower().startswith('/extras'):
+  if message.content.lower().startswith('!extras'):
     msg = config['extras']
     await client.send_message(message.channel, msg)
 
-  if message.content.lower().startswith('/community'):
+  if message.content.lower().startswith('!community'):
     msg = config['community']
     await client.send_message(message.channel, msg)
 
-  if message.content.lower().startswith('/resources'):
+  if message.content.lower().startswith('!platforms'):
+    msg = config['platforms']
+    await client.send_message(message.channel, msg)
+
+  if message.content.lower().startswith('!resources'):
     msg = config['resources']
     await client.send_message(message.channel, msg)
 
-  if message.content.lower().startswith('/conferences'):
+  if message.content.lower().startswith('!conferences'):
     msg = config['conferences']
     await client.send_message(message.channel, msg)
 
-  if message.content.lower().startswith('/previousevents'):
+  if message.content.lower().startswith('!previousevents'):
     msg = config['previousevents']
     await client.send_message(message.channel, msg)
 
-  if message.content.lower().startswith('/videos'):
+  if message.content.lower().startswith('!videos'):
     msg = config['videos']
     await client.send_message(message.channel, msg)
 
-  if message.content.lower().startswith('/uraiden'):
+  if message.content.lower().startswith('!uraiden'):
     msg = config['uraiden']
     await client.send_message(message.channel, msg)
 
-  if message.content.lower().startswith('/whenmoon'):
+  if message.content.lower().startswith('!whenmoon'):
     msg = config['whenmoon']
     await client.send_message(message.channel, msg)
 
-  if message.content.lower().startswith('/rules'):
+  if message.content.lower().startswith('!rules'):
     msg = config['rules']
     await client.send_message(message.channel, msg)
 
-  if message.content.lower().startswith('/pulse'):
+  if message.content.lower().startswith('!pulse'):
     msg = config['pulse']
     await client.send_message(message.channel, msg)
 
-  if message.content.lower().startswith('/nightly'):
+  if message.content.lower().startswith('!nightly'):
     msg = config['nightly']
     await client.send_message(message.channel, msg)
 
-  if message.content.lower().startswith('/release'):
+  if message.content.lower().startswith('!release'):
     msg = config['release']
     await client.send_message(message.channel, msg)
 
-  if message.content.lower().startswith('/email'):
+  if message.content.lower().startswith('!email'):
     msg = config['email']
     await client.send_message(message.channel, msg)
 
-  if message.content.lower().startswith('/brainbot'):
+  if message.content.lower().startswith('!brainbot'):
     msg = config['brainbot']
     await client.send_message(message.channel, msg)
 
-  if message.content.lower().startswith('/tokenmodel'):
+  if message.content.lower().startswith('!tokenmodel'):
     msg = config['tokenmodel']
     await client.send_message(message.channel, msg)
 
-  if message.content.lower().startswith('/disclaimer'):
+  if message.content.lower().startswith('!disclaimer'):
     msg = config['disclaimer']
     await client.send_message(message.channel, msg)
 
-  if message.content.lower().startswith('/rapps'):
+  if message.content.lower().startswith('!rapps'):
     msg = config['rapps']
     await client.send_message(message.channel, msg)
 
-  if message.content.lower().startswith('/mentions'):
-    msg = config['mentions']
-    await client.send_message(message.channel, msg)
+  if message.content.lower().startswith('!mentions'):
+   msg = config['mentions']
+   await client.send_message(message.channel, msg)
 
+#run bot
 client.run("TOKEN")
+client.http_session.close()
